dae67a90290d6e6bb7bfea2d5d8abfffa9fa06c8
carbonid1 <carbonid1@gmail.com>
2021-01-28 23:07:04 +0200

    add PWA support

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,6 @@
-import Head from "next/head";
-
 export default function Home() {
   return (
     <div>
-      <Head>
-        <title>Home</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
       <main>This is main</main>
       <footer>This is footer</footer>
     </div>
