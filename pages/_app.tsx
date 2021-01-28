50b7de7ecbfef0cf633f992a4cd59ed2d51975bb
carbonid1 <carbonid1@gmail.com>
2021-01-28 23:20:08 +0200

    add favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -20,7 +20,7 @@ function MyApp({ Component, pageProps }: AppProps) {
         <link href="/icons/favicon-32x32.png" rel="icon" type="image/png" sizes="32x32" />
         <link rel="apple-touch-icon" href="/apple-icon.png"></link>
         <meta name="theme-color" content="#317EFB" />
-        {/* <link rel="icon" href="/favicon.ico" /> */}
+        <link rel="icon" href="/favicon.ico" />
       </Head>
       <Component {...pageProps} />
     </>
