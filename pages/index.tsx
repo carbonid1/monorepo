a45809e8ee702f044cddbf1a07e9d37f22a836e5
carbonid1 <carbonid1@gmail.com>
2021-02-02 09:30:48 +0200

    add get book by id

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,34 @@
-import { Input } from "antd";
+import { withApollo } from '../apollo/client';
+import gql from 'graphql-tag';
+import { useQuery } from '@apollo/react-hooks';
 
-export default function Home() {
+const BookQ = gql`
+  query BookQ($bookId: Int!) {
+    book(bookId: $bookId) {
+      id
+      title
+      publishedIn
+      author
+    }
+  }
+`;
+
+const Home = () => {
+  const { loading, error, data } = useQuery(BookQ, {
+    variables: { bookId: 1 },
+  });
+
+  if (loading) return '...loading';
+  if (error) return 'error';
+
   return (
     <div>
-      <Input placeholder="Enter Name" />
-      <main>This is main</main>
-      <footer>This is footer</footer>
+      {data.book.id}
+      {data.book.title}
+      {data.book.publishedIn}
+      {data.book.author}
     </div>
   );
-}
+};
+
+export default withApollo(Home);
