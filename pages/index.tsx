be34cde2300c800a5560e3a58b2ff5e156346951
carbonid1 <carbonid1@gmail.com>
2021-02-08 00:00:06 +0200

    add list

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,37 @@
-const Home = () => {
-  return <h1>Welcome to BookHub</h1>;
+import { useQuery } from '@apollo/react-hooks';
+import gql from 'graphql-tag';
+import { withApollo } from '../apollo/client';
+import Link from 'next/link';
+import { ROUTE } from '../consts/routes';
+
+const BooksQ = gql`
+  query Books {
+    books {
+      slug
+      title
+      author
+    }
+  }
+`;
+
+const Home: React.FC = () => {
+  const { data } = useQuery(BooksQ);
+  const { books = [] } = data ?? {};
+
+  return (
+    <div>
+      {books.map((book: any) => (
+        <div>
+          <Link key={book.slug} href={`${ROUTE.book}/${book.slug}`}>
+            <a>
+              <b>{book.author}: </b>
+              {book.title}
+            </a>
+          </Link>
+        </div>
+      ))}
+    </div>
+  );
 };
 
-export default Home;
+export default withApollo(Home);
