30b4ea77be12de934054ac6c010b8aa816434849
carbonid1 <carbonid1@gmail.com>
2021-02-11 22:27:24 +0200

    config base url

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,5 +1,5 @@
+import { NotFound } from 'components/errors/NotFound';
 import React from 'react';
-import { NotFound } from '../components/errors/NotFound';
 
 const Page404: React.FC = () => {
   return <NotFound />;
