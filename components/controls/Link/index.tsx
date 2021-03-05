39715c568a1513587aede890912f72e4538de491
carbonid1 <carbonid1@gmail.com>
2021-03-05 08:56:29 +0200

    remove slug from DB

--- components/controls/Link/index.tsx
+++ components/controls/Link/index.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import NextLink, { LinkProps } from 'next/link';
+import slugify from 'slugify';
 
-export interface ILink extends LinkProps {
+export interface ILink extends Omit<LinkProps, 'href'> {
   children: React.ReactNode;
+  path: string;
+  slug?: string;
 }
 
-export const Link: React.FC<ILink> = ({ children, ...linkProps }) => {
+export const Link: React.FC<ILink> = ({ children, path, slug, ...linkProps }) => {
+  const href = slug ? `${path}.${slugify(slug, { lower: false })}` : path;
   return (
-    <NextLink {...linkProps}>
+    <NextLink {...linkProps} href={href}>
       <a className="text-blue-500 hover:underline">{children}</a>
     </NextLink>
   );
