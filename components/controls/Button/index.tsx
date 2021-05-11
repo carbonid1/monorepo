e95f7137743e25afbdd7e06275388d04ceb0427a
Andrew Korin <carbonid1@gmail.com>
2021-05-12 00:11:21 +0300

    init doc

--- components/controls/Button/index.tsx
+++ components/controls/Button/index.tsx
@@ -4,7 +4,7 @@ type ButtonT = JSX.IntrinsicElements['button'];
 
 export const Button: React.FC<ButtonT> = props => {
   return (
-    <button {...props} className="bg-green-700 p-3 text-white hover:opacity-90 rounded-md">
+    <button {...props} className="p-3 text-white hover:opacity-90 rounded-md">
       {props.children}
     </button>
   );
