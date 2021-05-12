fed835c1b2106324152c058be843bda5338b20ea
Andrew Korin <carbonid1@gmail.com>
2021-05-12 11:39:35 +0300

    upd storybook -v and add all1 addon

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   stories: ['../{components,modules}/**/*.stories.@(tsx|mdx)', './docs/**/*.stories.@(tsx|mdx)'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-a11y'],
   webpackFinal: config => ({
     ...config,
     resolve: {
