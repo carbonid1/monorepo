457c008a0990abafd9696d1f3682c6ab38ed11ae
Andrew Korin <carbonid1@gmail.com>
2021-05-15 22:46:28 +0300

    trancate head description content

--- components/CustomHead/index.tsx
+++ components/CustomHead/index.tsx
@@ -9,7 +9,12 @@ export const CustomHead: React.FC<ICustomHead> = ({ title, description }) => {
   return (
     <Head>
       <title key="title">{title} - BookHub</title>
-      {description && <meta name="description" key="description" content={description} />}
+      {description && <meta name="description" key="description" content={truncateDescription(description)} />}
     </Head>
   );
+};
+
+const truncateDescription = (description: string) => {
+  const maxLength = 152;
+  return description.length <= maxLength ? description : description.substring(0, maxLength) + '...';
 };
