3ddca0ba1f719167ac7050eed9d8c2074e8cdebf
Andrew Korin <carbonid1@gmail.com>
2021-05-18 00:04:17 +0300

    sticky header

--- modules/AppHeader/index.tsx
+++ modules/AppHeader/index.tsx
@@ -3,10 +3,15 @@ import React from 'react';
 
 export const AppHeader: React.FC = () => {
   return (
-    <div className="h-16 flex items-center justify-items-center p-8 w-full bg-gradient-to-b to-blue-600 from-green-500">
-      <Link href="/">
-        <a className="text-white text-3xl font-bold">BookHub</a>
-      </Link>
+    <div className="h-16">
+      <div
+        className="h-16 flex items-center justify-items-center p-8 
+        w-full bg-gradient-to-b to-blue-600 from-green-500 fixed"
+      >
+        <Link href="/">
+          <a className="text-white text-3xl font-bold">BookHub</a>
+        </Link>
+      </div>
     </div>
   );
 };
