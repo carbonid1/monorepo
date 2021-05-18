2a8c9d6315c16fd3983e4421a5413a2063a0e91c
Andrew Korin <carbonid1@gmail.com>
2021-05-18 19:55:28 +0300

    decouple component

--- modules/BookReviews/index.tsx
+++ modules/BookReviews/index.tsx
@@ -1,44 +1,18 @@
-import { gql, useQuery } from '@apollo/client';
-import { useMemo, useState } from 'react';
-import type { IBook, IEdition, IReview } from 'types/interfaces';
-import ISO6391 from 'iso-639-1';
+import { useState } from 'react';
+import type { IBook, IEdition } from 'types/interfaces';
+import { useReviewLangOptions } from './hooks';
+import { useReviewsQuery } from './queries';
 
 export interface IBookReviews {
   bookId: IBook['id'];
   editionId: IEdition['id'];
 }
-interface IQData {
-  reviews: IReview[];
-}
-interface IQVars {
-  bookId: number | null;
-  editionId: number | null;
-  lang: string | null;
-}
-
-const ReviewsQ = gql`
-  query EditionQ($bookId: ID, $editionId: ID, $lang: String) {
-    reviews(lang: $lang, bookId: $bookId, editionId: $editionId) {
-      body
-      lang
-      id
-      createdAt
-    }
-  }
-`;
 
 export const BookReviews: React.FC<IBookReviews> = ({ bookId, editionId }) => {
   const [thisEditionOnly, setThisEditionOnly] = useState(false);
   const [lang, setLang] = useState<string | null>(null);
-  const { data } = useQuery<IQData, IQVars>(ReviewsQ, {
-    variables: { bookId, editionId: thisEditionOnly ? editionId : null, lang },
-  });
-
-  const options = useMemo(() => {
-    const allOption = <option value="">All Languages</option>;
-    const langsOpts = data?.reviews.map(({ lang }) => <option value={lang}>{ISO6391.getName(lang)}</option>);
-    return [allOption, langsOpts];
-  }, []);
+  const { reviews } = useReviewsQuery({ bookId, editionId: thisEditionOnly ? editionId : null, lang });
+  const langOptions = useReviewLangOptions(reviews);
 
   return (
     <div>
@@ -52,7 +26,7 @@ export const BookReviews: React.FC<IBookReviews> = ({ bookId, editionId }) => {
           else setLang(value);
         }}
       >
-        {options}
+        {langOptions}
       </select>
       <label>
         <input
@@ -64,7 +38,7 @@ export const BookReviews: React.FC<IBookReviews> = ({ bookId, editionId }) => {
         This Edition
       </label>
       <div className="grid gap-2 mt-4">
-        {data?.reviews.map(review => (
+        {reviews.map(review => (
           <div key={review.id}>
             <div>{review.createdAt}</div>
             <div>{review.body}</div>
