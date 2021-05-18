7c698e66f846fffeda4acc78d0d394937247e205
Andrew Korin <carbonid1@gmail.com>
2021-05-18 18:02:49 +0300

    FE refactoring

--- modules/BookReviews/index.tsx
+++ modules/BookReviews/index.tsx
@@ -1,18 +1,38 @@
-import React, { useMemo } from 'react';
-import type { IEdition } from 'types/interfaces';
+import { gql, useQuery } from '@apollo/client';
+import type { IBook, IEdition, IReview } from 'types/interfaces';
 
 export interface IBookReviews {
-  editions: IEdition[];
+  bookId: IBook['id'];
+  editionId: IEdition['id'];
 }
-export const BookReviews: React.FC<IBookReviews> = ({ editions }) => {
-  const allReviews = useMemo(
-    () => editions.reduce((reviews: typeof edition.reviews, edition) => [...reviews, ...edition.reviews], []),
-    [editions]
-  );
+interface IQData {
+  reviews: IReview[];
+}
+interface IQVars {
+  bookId: number | null;
+  editionId: number | null;
+  lang: string | null;
+}
+
+const ReviewsQ = gql`
+  query EditionQ($bookId: ID, $editionId: ID, $lang: String) {
+    reviews(lang: $lang, bookId: $bookId, editionId: $editionId) {
+      body
+      lang
+      id
+      createdAt
+    }
+  }
+`;
+
+export const BookReviews: React.FC<IBookReviews> = ({ bookId, editionId }) => {
+  const { data } = useQuery<IQData, IQVars>(ReviewsQ, {
+    variables: { bookId, editionId, lang: null },
+  });
 
   return (
     <div className="grid py-4 gap-2">
-      {allReviews.map(review => (
+      {data?.reviews.map(review => (
         <div key={review.id}>
           <div>{review.createdAt}</div>
           <div>{review.body}</div>
