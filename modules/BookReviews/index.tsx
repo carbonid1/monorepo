fd1b08f3032dec7db85c2b7b0191facd6b0bd4ce
Andrew Korin <carbonid1@gmail.com>
2021-05-18 18:20:08 +0300

    this edition filter

--- modules/BookReviews/index.tsx
+++ modules/BookReviews/index.tsx
@@ -1,4 +1,5 @@
 import { gql, useQuery } from '@apollo/client';
+import { useState } from 'react';
 import type { IBook, IEdition, IReview } from 'types/interfaces';
 
 export interface IBookReviews {
@@ -26,18 +27,34 @@ const ReviewsQ = gql`
 `;
 
 export const BookReviews: React.FC<IBookReviews> = ({ bookId, editionId }) => {
+  const [thisEditionOnly, setThisEditionOnly] = useState(false);
   const { data } = useQuery<IQData, IQVars>(ReviewsQ, {
-    variables: { bookId, editionId, lang: null },
+    variables: { bookId, editionId: thisEditionOnly ? editionId : null, lang: null },
   });
 
+  if (!data?.reviews.length) return null;
+
   return (
-    <div className="grid py-4 gap-2">
-      {data?.reviews.map(review => (
-        <div key={review.id}>
-          <div>{review.createdAt}</div>
-          <div>{review.body}</div>
-        </div>
-      ))}
+    <div>
+      <div className="font-bold text-2xl py-4">Reviews:</div>
+      <label htmlFor="this-edition-toggle">
+        <input
+          type="checkbox"
+          checked={thisEditionOnly}
+          id="this-edition-toggle"
+          onChange={e => setThisEditionOnly(e.target.checked)}
+          className="mr-2"
+        />
+        This Edition
+      </label>
+      <div className="grid gap-2">
+        {data?.reviews.map(review => (
+          <div key={review.id}>
+            <div>{review.createdAt}</div>
+            <div>{review.body}</div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
