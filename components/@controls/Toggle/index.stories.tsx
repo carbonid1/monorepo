8244b70bfc75781f0a803306dc40a1b2a2a4593d
Andrii Korin <36999873+carbonid1@users.noreply.github.com>
2021-05-30 16:30:09 +0300

    Switch (#8)
    * initial implementation
    
    * add label positon into storybook
    
    * use new Toggle for reviews

---
+++ components/@controls/Toggle/index.stories.tsx
@@ -0,0 +1,18 @@
+import type { Story, Meta } from '@storybook/react';
+import { useState } from 'react';
+import { Toggle, IToggle } from '.';
+
+export default { title: '@controls/Toggle', component: Toggle } as Meta;
+const Template: Story<IToggle> = props => {
+  const [isChecked, onChange] = useState(props.isChecked);
+  return <Toggle {...props} isChecked={isChecked} onChange={onChange} />;
+};
+
+export const Default = Template.bind({});
+Default.args = { isChecked: true, srLabel: 'Show All', labelPosition: 'right' };
+
+export const WithLabel = Template.bind({});
+WithLabel.args = { ...Default.args, label: 'Show All' };
+
+// export const WithLabelAndDescription = Template.bind({});
+// WithLabelAndDescription.args = { ...WithLabel.args, description: 'Shows all settings' };
