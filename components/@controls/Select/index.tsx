942cf4ceadbf068789cbff33f16c888e96491442
Andrii Korin <36999873+carbonid1@users.noreply.github.com>
2021-06-02 21:32:21 +0300

    Select (#12)
    * base Select
    
    * integrate into edition reviews
    
    * add a state for the select story
    
    * update height for edition reivews
    
    * rm number as default type for ISelect

---
+++ components/@controls/Select/index.tsx
@@ -0,0 +1,67 @@
+import { Fragment, useMemo } from 'react';
+import { Listbox, Transition } from '@headlessui/react';
+import { CheckIcon, SelectorIcon } from '@heroicons/react/solid';
+import cn from 'classnames';
+
+interface ISelectOption<V> {
+  label: string;
+  value: V;
+}
+export interface ISelect<V> {
+  options: ISelectOption<V>[];
+  placeholder?: string;
+  value: ISelectOption<V>['value'];
+  onChange: (value: ISelectOption<V>['value']) => void;
+}
+
+export const Select = <V,>({ options, placeholder = 'Select an Option', onChange, value }: ISelect<V>) => {
+  const selectedOption = useMemo(() => {
+    return options.find(option => option.value === value);
+  }, [options, value]);
+
+  return (
+    <Listbox value={value} onChange={onChange}>
+      <div className="relative w-52">
+        <Listbox.Button className="relative w-full py-2 pl-3 pr-10 text-left bg-white rounded-lg shadow-md cursor-default focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-400 sm:text-sm">
+          {selectedOption ? (
+            <span className="block truncate">{selectedOption.label}</span>
+          ) : (
+            <span className="block truncate text-grey-400">{placeholder}</span>
+          )}
+          <span className="absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none">
+            <SelectorIcon className="w-5 h-5 text-grey-400" aria-hidden="true" />
+          </span>
+        </Listbox.Button>
+        <Transition as={Fragment} leave="transition ease-in duration-100" leaveFrom="opacity-100" leaveTo="opacity-0">
+          <Listbox.Options className="absolute w-full py-1 mt-1 overflow-auto text-base bg-white rounded-md shadow-lg max-h-60 focus:outline-none focus:ring-2 focus:ring-blue-400 sm:text-sm">
+            {options.map((option, index) => (
+              <Listbox.Option
+                key={index}
+                className={({ active }) =>
+                  cn(
+                    active ? 'text-blue-500 bg-blue-100' : 'text-grey-900',
+                    'cursor-default select-none relative py-2 pl-10 pr-4'
+                  )
+                }
+                value={option.value}
+              >
+                {({ selected }) => (
+                  <>
+                    <span className={cn(selected ? 'font-medium' : 'font-normal', 'block truncate')}>
+                      {option.label}
+                    </span>
+                    {selected && (
+                      <span className="text-blue-600 absolute inset-y-0 left-0 flex items-center pl-3">
+                        <CheckIcon className="w-5 h-5" aria-hidden="true" />
+                      </span>
+                    )}
+                  </>
+                )}
+              </Listbox.Option>
+            ))}
+          </Listbox.Options>
+        </Transition>
+      </div>
+    </Listbox>
+  );
+};
--- modules/BookReviews/index.tsx
+++ modules/BookReviews/index.tsx
@@ -1,9 +1,10 @@
+import { ISelect, Select } from 'components/@controls/Select';
 import { Toggle } from 'components/@controls/Toggle';
 import { useState } from 'react';
 import type { IBook, IEdition } from 'types/interfaces';
 import formatDate from 'utils/formatDate';
-import { useReviewLangOptions } from './hooks';
-import { useReviewsQuery } from './queries';
+import hooks from './hooks';
+import queries from './queries';
 
 export interface IBookReviews {
   bookId: IBook['id'];
@@ -12,28 +13,18 @@ export interface IBookReviews {
 
 export const BookReviews: React.FC<IBookReviews> = ({ bookId, editionId }) => {
   const [thisEditionOnly, setThisEditionOnly] = useState(false);
-  const [lang, setLang] = useState<string | null>(null);
-  const { reviews } = useReviewsQuery({ bookId, editionId: thisEditionOnly ? editionId : null, lang });
-  const langOptions = useReviewLangOptions(reviews);
+  const [lang, setLang] = useState<ISelect<string | null>['value']>(null);
+  const { reviews } = queries.useReviewsQuery({ bookId, editionId: thisEditionOnly ? editionId : null, lang });
+  const langOptions = hooks.useLangOptions(reviews);
 
   return (
     <div>
       <div className="font-bold text-2xl py-4">Reviews:</div>
       <div className="flex gap-x-4">
-        <select
-          name="languages"
-          id="lang-select"
-          className="mr-2"
-          onChange={({ target: { value } }) => {
-            if (value === '') return setLang(null);
-            else setLang(value);
-          }}
-        >
-          {langOptions}
-        </select>
+        <Select options={langOptions} value={lang} onChange={setLang} />
         <Toggle isChecked={thisEditionOnly} onChange={setThisEditionOnly} label="This Edition Only" />
       </div>
-      <div className="grid gap-2 mt-4">
+      <div className="grid gap-2 auto-rows-max mt-4 min-h-[400px]">
         {reviews.map(review => (
           <div key={review.id}>
             <div>{formatDate(review.createdAt)}</div>
