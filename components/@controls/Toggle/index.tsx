46814fe350dc32dadf760514408d5fb56493e1ed
Andrii Korin <36999873+carbonid1@users.noreply.github.com>
2021-06-04 00:07:29 +0300

    Pre commit hooks (#15)
    * init
    
    * add jest
    
    * prettier setup
    
    * upd changes
    
    * lint staged with prettier
    
    * eslint
    
    * add ci
    
    * upd tabs

--- components/@controls/Toggle/index.tsx
+++ components/@controls/Toggle/index.tsx
@@ -36,7 +36,7 @@ export const Toggle: React.FC<IToggle> = ({
         checked={isChecked}
         className={cn(
           isChecked ? 'bg-blue-400' : 'bg-grey-200',
-          'relative inline-flex flex-shrink-0 h-6 w-11 border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-400'
+          'relative inline-flex flex-shrink-0 h-6 w-11 border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-400',
         )}
       >
         {srLabel && !label && <span className="sr-only">{srLabel}</span>}
@@ -44,7 +44,7 @@ export const Toggle: React.FC<IToggle> = ({
           aria-hidden
           className={cn(
             isChecked ? 'translate-x-5' : 'translate-x-0',
-            'pointer-events-none inline-block h-5 w-5 rounded-full bg-white shadow transform ring-0 transition ease-in-out duration-200'
+            'pointer-events-none inline-block h-5 w-5 rounded-full bg-white shadow transform ring-0 transition ease-in-out duration-200',
           )}
         />
       </Switch>
--- mocks/reviews.mock.ts
+++ mocks/reviews.mock.ts
@@ -14,14 +14,12 @@ const initialMock: Record<TReviews, IInitialMock> = {
   rangeRu1: {
     lang: 'ru',
     createdAt: '2021-02-24',
-    body:
-      'Отличная книжка (и не только про/для консультантов, хотя BCG & McK по разу упоминаются). Мне больше всего понравилось про "поздний старт" в новой деятельности, почему он может быть успешным и вообще про смену карьеры, и глава с развенчанием мифа про grit и persistence (не Angela Duckworth единой, в общем). Находящиеся в процессе смены вида деятельности (aka career changers) могут цитировать куски на собеседованиях, если вдруг кто усомнится в пользе их прошлого опыта ("no experience is wasted"). И родителям тоже полезно (Рихтер начал брать нормальные уроки музыки в 22 и вполне себе преуспел, можно не мучить ребенка сольфеджио и скрипкой в 8).',
+    body: 'Отличная книжка (и не только про/для консультантов, хотя BCG & McK по разу упоминаются). Мне больше всего понравилось про "поздний старт" в новой деятельности, почему он может быть успешным и вообще про смену карьеры, и глава с развенчанием мифа про grit и persistence (не Angela Duckworth единой, в общем). Находящиеся в процессе смены вида деятельности (aka career changers) могут цитировать куски на собеседованиях, если вдруг кто усомнится в пользе их прошлого опыта ("no experience is wasted"). И родителям тоже полезно (Рихтер начал брать нормальные уроки музыки в 22 и вполне себе преуспел, можно не мучить ребенка сольфеджио и скрипкой в 8).',
   },
   rangeRu2: {
     lang: 'en',
     createdAt: '2021-03-20',
-    body:
-      "Do I think it's a five-star book? It's very hard for me to say, as I wrote the thing. By the time I'm done working on a book, I have such a strong insider view of the project that it's difficult to be objective. I will say this: I worked extremely hard on it, and as a writer, researcher, and reader, I found it to be much more interesting than my first book. Most readers enjoyed that first book--at least according to Goodreads ratings--so I hope most readers will (as I have) enjoy this one even more.",
+    body: "Do I think it's a five-star book? It's very hard for me to say, as I wrote the thing. By the time I'm done working on a book, I have such a strong insider view of the project that it's difficult to be objective. I will say this: I worked extremely hard on it, and as a writer, researcher, and reader, I found it to be much more interesting than my first book. Most readers enjoyed that first book--at least according to Goodreads ratings--so I hope most readers will (as I have) enjoy this one even more.",
   },
 };
 
