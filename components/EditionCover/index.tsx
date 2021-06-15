5a78fd26fa8c3e3926325f94d54a96de8cc18db9
Andrew Korin <carbonid1@gmail.com>
2021-06-15 14:04:26 +0300

    init styles

--- components/EditionCover/index.tsx
+++ components/EditionCover/index.tsx
@@ -1,10 +1,18 @@
 import type { IEdition } from 'types/interfaces';
 import cn from 'classnames';
+import { motion } from 'framer-motion';
 
 export interface IEditionCover extends Pick<IEdition, 'cover' | 'title'> {
   className?: string;
 }
 
 export const EditionCover: React.FC<IEditionCover> = ({ cover, title, className }) => {
-  return <img src={cover} alt={title} className={cn('bg-blue-300 h-56 w-40', className)} />;
+  return (
+    <motion.img
+      src={cover}
+      alt={title}
+      whileTap={{ scale: 2, x: '50%', y: '50%', animationDelay: '6s' }}
+      className={cn('bg-blue-300 cursor-pointer h-56 w-40 rounded-2xl', className)}
+    />
+  );
 };
