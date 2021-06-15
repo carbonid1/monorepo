db3d24291ce8cd1e851ebf71b1cdf16baf6b2be8
Andrew Korin <carbonid1@gmail.com>
2021-06-15 15:37:13 +0300

    use edition cover for author image

--- components/EditionCover/index.tsx
+++ components/EditionCover/index.tsx
@@ -12,7 +12,7 @@ export const EditionCover: React.FC<IEditionCover> = ({ cover, title, className 
       src={cover}
       alt={title}
       whileTap={{ scale: 2, x: '50%', y: '50%' }}
-      className={cn('bg-blue-300 cursor-pointer h-56 w-40 z-1', className)}
+      className={cn('cursor-pointer w-40 z-1 object-contain', className)}
     />
   );
 };
