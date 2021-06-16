e9df7302f6a8dd3ea9c29959cd0683e815d66428
Andrew Korin <carbonid1@gmail.com>
2021-06-16 21:50:58 +0300

    add app block and emeral colors set

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -29,9 +29,11 @@ function MyApp({ Component, pageProps }: AppProps) {
         <meta name="theme-color" content="#317EFB" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <AppHeader />
-      <div className="p-4 max-w-5xl mx-auto">
-        <Component {...pageProps} />
+      <div className="light-blue" id="app">
+        <AppHeader />
+        <div className="max-w-5xl p-4 mx-auto">
+          <Component {...pageProps} />
+        </div>
       </div>
     </>
   );
