d38743be0fc0171239af455feee39f9bdf6c3923
Andrew Korin <carbonid1@gmail.com>
2021-08-05 23:53:40 +0300

    Maybe type, fix ts

--- utils/formatDate/index.ts
+++ utils/formatDate/index.ts
@@ -1,7 +1,7 @@
 import fromUnixTime from 'date-fns/fromUnixTime';
 import format from 'date-fns/format';
 
-const formatDate = (timestamp: string | undefined) => {
+const formatDate = (timestamp: Maybe<string>) => {
   if (!timestamp) return 'N/A';
   return format(fromUnixTime(+timestamp / 1000), 'MMMM do y');
 };
