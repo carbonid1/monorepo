67a9fb9b12df390b84f508a9454e29ea14809b46
Andrew Korin <carbonid1@gmail.com>
2021-08-23 16:11:06 +0300

    upd

--- pages/review/[id].tsx
+++ pages/review/[id].tsx
@@ -1,1 +1,19 @@
+import { ReviewPage_ReviewDocument, ReviewPage_ReviewQuery, ReviewPage_ReviewQueryVariables } from 'generated/graphql';
+import { initializeApollo } from 'lib/apollo';
+import type { GetServerSideProps } from 'next';
+
 export { default as default } from 'modules/ReviewPage';
+
+export const getServerSideProps: GetServerSideProps = async ({ query }) => {
+  const id = query.id as string;
+
+  const apolloClient = initializeApollo();
+  await apolloClient.query<ReviewPage_ReviewQuery, ReviewPage_ReviewQueryVariables>({
+    query: ReviewPage_ReviewDocument,
+    variables: { id },
+  });
+
+  return {
+    props: { initialApolloState: apolloClient.cache.extract(), id },
+  };
+};
