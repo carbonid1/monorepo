128c451d0ead198aec734e9fac1c2f5e83c4b5d2
Andrii Korin <36999873+carbonid1@users.noreply.github.com>
2021-08-26 22:20:19 +0300

    auth session using provider pattern (#49)
    * auth session using provider pattern
    
    * ui updates

--- next.config.js
+++ next.config.js
@@ -11,5 +11,8 @@ module.exports = withBundleAnalyzer(
       runtimeCaching,
       disable: process.env.NODE_ENV === 'development',
     },
+    images: {
+      domains: ['lh3.googleusercontent.com'],
+    },
   }),
 );
