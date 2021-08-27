2f556860ccf1403f520f8cabf50dff354bf028f6
Andrii Korin <36999873+carbonid1@users.noreply.github.com>
2021-08-27 22:50:13 +0300

    Custom sign in page (#50)
    * init
    
    * redirects
    
    * add spacing
    
    * button styles

---
+++ lib/modules/AuthPages/SignInPage/index.tsx
@@ -0,0 +1,44 @@
+import { GoogleIcon } from 'lib/icons';
+import type { GetServerSideProps } from 'next';
+import { ClientSafeProvider, getProviders, getSession, signin } from 'next-auth/client';
+
+export interface ISignInPage {
+  providers: Record<'google', ClientSafeProvider>;
+}
+
+const SignInPage: React.FC<ISignInPage> = ({ providers }) => {
+  const googleProvider = providers?.google;
+
+  return (
+    <div className="flex items-center justify-center flex-1">
+      <button
+        onClick={() => signin(googleProvider.id)}
+        className="flex items-center w-full max-w-xs p-4 text-xl font-medium shadow-md rounded-xl text-grey-600"
+      >
+        <GoogleIcon className="mr-4 text-2xl" />
+        Continue with Google
+      </button>
+    </div>
+  );
+};
+
+export const getServerSideProps: GetServerSideProps = async ctx => {
+  const session = await getSession(ctx);
+
+  if (session) {
+    return {
+      props: {},
+      redirect: {
+        destination: ctx.query.callbackUrl || '/',
+      },
+    };
+  }
+
+  return {
+    props: {
+      providers: await getProviders(),
+    },
+  };
+};
+
+export default SignInPage;
