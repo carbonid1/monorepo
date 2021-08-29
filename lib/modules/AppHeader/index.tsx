4d3ea0a882f6529b02fbf7eb1ed59ba0fa45d507
Andrew Korin <carbonid1@gmail.com>
2021-08-29 10:40:51 +0300

    fix app header color

--- lib/modules/AppHeader/index.tsx
+++ lib/modules/AppHeader/index.tsx
@@ -8,11 +8,11 @@ export const AppHeader: React.FC = () => {
   return (
     <div className="sticky top-0 flex items-center w-full h-16 bg-blue-600 bg-opacity-50 backdrop-filter backdrop-blur z-header justify-items-center">
       <div className="flex items-center flex-1 max-w-5xl p-4 mx-auto">
-        <Link path="/" className="font-bold text-white mr-auto text-3xl">
+        <Link path="/" className="font-bold mr-auto text-3xl" color="accent">
           BookHub
         </Link>
         {user?.image && <NextImg className="rounded-full" src={user?.image} alt="profile" height={48} width={48} />}
-        <Link className="font-bold text-white ml-4" onClick={user ? () => signOut() : () => signIn()}>
+        <Link className="font-bold ml-4" color="accent" onClick={user ? () => signOut() : () => signIn()}>
           {user ? 'Sign Out' : 'Sign In'}
         </Link>
       </div>
