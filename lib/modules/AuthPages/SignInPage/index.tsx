d96c9e7a19ba561c1f114218e3b1da8c0ed4d6fe
Andrii Korin <36999873+carbonid1@users.noreply.github.com>
2021-09-03 09:23:48 +0300

    Twitter authentication (#58)

--- lib/modules/AuthPages/SignInPage/index.tsx
+++ lib/modules/AuthPages/SignInPage/index.tsx
@@ -1,31 +1,35 @@
 import type { GetServerSideProps } from 'next';
 import { ClientSafeProvider, getProviders, getSession, signin } from 'next-auth/client';
-import { GoogleIcon, GitHubIcon } from 'lib/icons';
+import { GoogleIcon, GitHubIcon, TwitterIcon } from 'lib/icons';
 
 export interface ISignInPage {
-  providers: Record<'google' | 'github', ClientSafeProvider>;
+  providers: Record<'google' | 'github' | 'twitter', ClientSafeProvider>;
 }
 
 const SignInPage: React.FC<ISignInPage> = ({ providers }) => {
-  const googleProvider = providers?.google;
-  const githubProvider = providers?.github;
-
   return (
     <div className="flex items-center justify-center flex-1">
       <div className="grid gap-4 p-6 transition-transform duration-300 shadow-md rounded-xl focus-within:shadow-xl focus-within:translate-y-[-4px] focus-within:transform">
         <button
-          onClick={() => signin(googleProvider.id)}
+          onClick={() => signin(providers?.google.id)}
           className="flex items-center w-full max-w-xs p-4 text-xl font-medium rounded-xl text-grey-600"
         >
           <GoogleIcon className="mr-4 text-2xl" />
           Continue with Google
         </button>
         <button
-          onClick={() => signin(githubProvider.id)}
+          onClick={() => signin(providers?.github.id)}
           className="flex items-center w-full max-w-xs p-4 text-xl font-medium rounded-xl text-grey-600"
         >
           <GitHubIcon className="mr-4 text-2xl" />
           Continue with GitHub
+        </button>
+        <button
+          onClick={() => signin(providers?.twitter.id)}
+          className="flex items-center w-full max-w-xs p-4 text-xl font-medium rounded-xl text-grey-600"
+        >
+          <TwitterIcon className="mr-4 text-2xl" />
+          Continue with Twitter
         </button>
       </div>
     </div>
