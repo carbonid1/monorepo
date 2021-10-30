14cb7385fe97383b76a4b5c1f7debd9bc8ba8529
Andrew Korin <36999873+carbonid1@users.noreply.github.com>
2021-10-30 13:17:40 +0300

    Toggle component based on Radix-UI (#72)

--- lib/components/@controls/Toggle/index.tsx
+++ lib/components/@controls/Toggle/index.tsx
@@ -1,65 +1,33 @@
 import cn from 'classnames';
-import { Switch } from '@headlessui/react';
+import * as Switch from '@radix-ui/react-switch';
+import type { SwitchProps } from '@radix-ui/react-switch';
 
-export interface IToggle {
-  className?: string;
-  onChange: (isChecked: boolean) => void;
-  isChecked: boolean;
-  label?: React.ReactNode;
-  srLabel?: string;
-  isPassiveLabel?: boolean;
-  labelPosition?: 'left' | 'right';
+export interface IToggle extends SwitchProps {
+  label?: string;
 }
 
-export const Toggle: React.FC<IToggle> = ({
-  label,
-  srLabel,
-  onChange,
-  className,
-  isChecked,
-  isPassiveLabel,
-  labelPosition = 'right',
-}) => {
+export const Toggle: React.FC<IToggle> = ({ label, className, checked, ...props }) => {
   return (
-    <Switch.Group as="div" className={cn('inline-flex gap-x-2 items-center justify-between', className)}>
-      {label && labelPosition === 'left' && (
-        <Switch.Label passive={isPassiveLabel} className="text-sm font-medium text-skin-complement">
+    <div className={cn('inline-flex gap-x-2 items-center justify-between', className)}>
+      {label && (
+        <label htmlFor={props.id} className="text-sm font-medium text-skin-complement">
           {label}
-        </Switch.Label>
+        </label>
       )}
-      <Switch
-        onChange={onChange}
-        checked={isChecked}
+      <Switch.Root
+        {...props}
         className={cn(
-          isChecked ? 'bg-skin-button-base' : 'bg-[#e5e7eb] dark:bg-skin-complement',
+          checked ? 'bg-skin-button-base' : 'bg-[#e5e7eb] dark:bg-skin-complement',
           'relative inline-flex flex-shrink-0 h-6 w-11 border-2 border-skin-base rounded-full transition-colors ease-in-out duration-200',
         )}
       >
-        {srLabel && !label && <span className="sr-only">{srLabel}</span>}
-        <span
-          aria-hidden
+        <Switch.Thumb
           className={cn(
-            isChecked ? 'translate-x-5' : 'translate-x-0',
+            checked ? 'translate-x-5' : 'translate-x-0',
             'pointer-events-none inline-block h-5 w-5 rounded-full bg-[white] shadow transform ring-0 transition ease-in-out duration-200',
           )}
         />
-      </Switch>
-      {label && labelPosition === 'right' && (
-        <Switch.Label passive={isPassiveLabel} className="text-sm font-medium text-skin-complement">
-          {label}
-        </Switch.Label>
-      )}
-    </Switch.Group>
+      </Switch.Root>
+    </div>
   );
 };
-
-Toggle.propTypes = {
-  label: function (props) {
-    if (!props.srLabel && !props.label) {
-      return new Error('Please provide either a label or a screen reader label!');
-    } else return null;
-  },
-};
-
-// * 1) I can decouple group, label and description from the switch
-// *   if I would like to use Swith without those particular components
