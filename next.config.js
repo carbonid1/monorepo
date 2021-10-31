103ecf0809fbf73dadb8a55b5013d0cc4aa8376e
Andrew Korin <carbonid1@gmail.com>
2021-10-31 17:10:46 +0200

    init

--- next.config.js
+++ next.config.js
@@ -9,7 +9,7 @@ module.exports = withBundleAnalyzer(
     pwa: {
       dest: 'public',
       runtimeCaching,
-      disable: process.env.NODE_ENV === 'development',
+      // disable: process.env.NODE_ENV === 'development',
     },
     images: {
       domains: [
