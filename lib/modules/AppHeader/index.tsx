59dff54f64b26f5d80878f6231e1faa4d4f86770
Andrew Korin <carbonid1@gmail.com>
2021-11-01 08:35:32 +0200

    add sign in and out loader

--- lib/modules/AppHeader/index.tsx
+++ lib/modules/AppHeader/index.tsx
@@ -1,10 +1,22 @@
 import { signIn, signOut } from 'next-auth/client';
 import { TextLink } from 'lib/components';
 import useProfile from 'lib/hooks/useProfile';
+import { useAppProgress } from '../AppProgress/useAppProgress';
 import { HeaderAvatar } from './HeaderAvatar';
 
 export const AppHeader: React.FC = () => {
+  const setIsAnimating = useAppProgress(state => state.setIsAnimating);
   const { profile } = useProfile();
+
+  const handleSignOut = () => {
+    setIsAnimating(true);
+    signOut().then(() => setIsAnimating(false));
+  };
+
+  const handleSignIn = () => {
+    setIsAnimating(true);
+    signIn().then(() => setIsAnimating(false));
+  };
 
   return (
     <div className="sticky top-0 flex items-center w-full bg-skin-base z-header justify-items-center">
@@ -13,7 +25,7 @@ export const AppHeader: React.FC = () => {
           BookHub
         </TextLink>
         <HeaderAvatar />
-        <TextLink className="ml-4 font-bold" color="text" onClick={profile ? () => signOut() : () => signIn()}>
+        <TextLink className="ml-4 font-bold" color="text" onClick={profile ? handleSignOut : handleSignIn}>
           {profile ? 'Sign Out' : 'Sign In'}
         </TextLink>
       </div>
