c5e4c4ea51a8f0acba5438f1c087d5d0c8a46fd3
Andrew Korin <carbonid1@gmail.com>
2021-11-01 08:09:06 +0200

    upd border radius

--- lib/modules/AppProgress/index.tsx
+++ lib/modules/AppProgress/index.tsx
@@ -13,10 +13,9 @@ export const AppProgress: React.FC = () => {
   return (
     <Progress.Root value={progressInPercentage} className="sticky top-0 w-full z-progress">
       <Progress.Indicator
-        className={cn('h-1 rounded-tr-sm rounded-br-sm bg-skin-primary', isFinished ? 'opacity-0' : 'opacity-100')}
+        className={cn('h-1 rounded-tr-lg rounded-br-lg bg-skin-primary', isFinished ? 'opacity-0' : 'opacity-100')}
         style={{
           height: 3,
-          borderRadius: '0 30% 30% 0',
           width: `${progressInPercentage}%`,
           transition: `width ${animationDuration}ms cubic-bezier(0.65, 0, 0.35, 1)`,
         }}
