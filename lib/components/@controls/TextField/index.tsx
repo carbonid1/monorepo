01b533f87ea1d4ef3949d4b8818ef18fbb28f8f6
Andrew Korin <carbonid1@gmail.com>
2021-11-17 02:08:44 +0200

    init form

---
+++ lib/components/@controls/TextField/index.tsx
@@ -0,0 +1,16 @@
+export interface TextFieldProps {
+	className?: string;
+	inputProps?: JSX.IntrinsicElements['input'];
+	label?: React.ReactNode;
+}
+export const TextField: React.FC<TextFieldProps> = ({ className, inputProps, label }) => {
+	return (
+		<fieldset className={className}>
+			{label && <label htmlFor={inputProps?.id}>{label}</label>}
+			<input
+				{...inputProps}
+				className="border-2 rounded-md focus:ring-2 focus:ring-skin-primary focus:outline-none bg-skin-complement border-skin-base"
+			/>
+		</fieldset>
+	);
+};
--- lib/modules/SettingsPage/index.tsx
+++ lib/modules/SettingsPage/index.tsx
@@ -1,19 +1,41 @@
 import type { GetServerSideProps, NextPage } from 'next';
 import { getSession } from 'next-auth/client';
+import { useForm } from 'react-hook-form';
 import { CustomHead } from 'lib/components/CustomHead';
 import { ROUTE } from 'lib/consts/routes';
 import { useUpdateProfile } from 'lib/hooks/updateProfile';
 import useProfile from 'lib/hooks/useProfile';
+import { TextField } from 'lib/components/@controls/TextField';
+
+interface FormData {
+	name: string | null;
+}
 
 const SettingsPage: NextPage = () => {
 	const { profile } = useProfile();
 	const { updateProfile } = useUpdateProfile();
+	const { register, handleSubmit } = useForm<FormData>({ defaultValues: { name: profile?.name } });
+	const onSubmit = handleSubmit(variables => {
+		updateProfile({ variables });
+	});
 
 	return (
 		<>
 			<CustomHead title="Settings" />
-			<button onClick={() => updateProfile({ variables: { name: 'hello' } })}>Settings</button>
+
 			<pre>{JSON.stringify(profile, null, 2)}</pre>
+
+			<form onSubmit={onSubmit} className="mt-10" noValidate={false}>
+				<TextField
+					label="Name"
+					inputProps={{
+						id: 'name',
+						type: 'text',
+						...register('name', { required: true }),
+					}}
+				/>
+				<button type="submit">Submit</button>
+			</form>
 		</>
 	);
 };
