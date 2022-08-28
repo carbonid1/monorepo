0bb6ea227f3cfba8f44d7b6faaea9f3284b80a58
Andrew Korin <carbonid1@gmail.com>
2022-08-28 07:58:57 +0300

    form inputs ups

--- apps/notion-api/pages/api/day/getInputs/formInputs.ts
+++ apps/notion-api/pages/api/day/getInputs/formInputs.ts
@@ -2,6 +2,7 @@ import { formatRFC3339 } from 'date-fns'
 
 type Params = {
   timezone: string
+  hour: number
 }
 type Response = {
   calendarEvents: {
@@ -11,9 +12,9 @@ type Response = {
 }
 type FormInputs = (params: Params) => Response
 
-export const formInputs: FormInputs = ({ timezone }) => {
+export const formInputs: FormInputs = ({ timezone, hour }) => {
   process.env.TZ = timezone
-  const timeMin = formatRFC3339(new Date().setHours(0, 0, 0, 0))
+  const timeMin = formatRFC3339(new Date().setHours(0, hour, 0, 0))
   const timeMax = formatRFC3339(new Date().setHours(23, 59, 59, 999))
 
   return {
