42d3af1ff13232834ac11ebfd28973bcf3f8508a
Andrew Korin <36999873+carbonid1@users.noreply.github.com>
2023-01-09 12:47:40 +0200

    refactor: remove sending email on entry creation (#202)
    * Remove sending email on entry creation
    
    * upd turbo

--- apps/notion-api/consts/index.ts
+++ apps/notion-api/consts/index.ts
@@ -1,4 +1,5 @@
 export * from './my-notion'
 export * from './pipedream'
 export * from './currency-codes'
+
 export const isProduction = process.env.VERCEL_ENV === 'production'
--- apps/notion-api/pages/api/journal/createDailyEntry.ts
+++ apps/notion-api/pages/api/journal/createDailyEntry.ts
@@ -1,5 +1,4 @@
 import { GoogleCalendarsResponse } from 'lib/interfaces'
-import { sendEmail } from './sendEmail'
 import { createNotionPage } from './createNotionPage'
 import { getCelebrations } from './getCelebrations'
 import { getWorkEvents } from './getWorkEvents'
@@ -7,7 +6,6 @@ import { getHabits } from './getHabits'
 import { getEngTodos } from './getEngTodos'
 import { getWorkTodos } from './getWorkTodos'
 import { getPersonalTodos } from './getPersonalTodos'
-import { isProduction } from 'consts'
 
 interface Params {
   workEvents: GoogleCalendarsResponse
@@ -18,7 +16,7 @@ interface Params {
 export const createDailyEntry = async ({ celebrationEvents, workEvents, timezone }: Params) => {
   process.env.TZ = timezone
 
-  const createPageResponse = await createNotionPage([
+  await createNotionPage([
     ...(await getHabits()),
     ...(await getPersonalTodos()),
     ...getCelebrations(celebrationEvents),
@@ -26,8 +24,4 @@ export const createDailyEntry = async ({ celebrationEvents, workEvents, timezone
     ...getWorkEvents(workEvents),
     ...(await getWorkTodos()),
   ])
-
-  if (isProduction) {
-    await sendEmail('url' in createPageResponse ? createPageResponse.url : null)
-  }
 }
