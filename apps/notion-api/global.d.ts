670339434f83fb63664f6eb2c9258fd5f370662a
Andrew Korin <carbonid1@gmail.com>
2023-01-09 12:39:51 +0200

    Remove sending email on entry creation

--- apps/notion-api/global.d.ts
+++ apps/notion-api/global.d.ts
@@ -2,9 +2,6 @@ declare namespace NodeJS {
   export interface ProcessEnv {
     NOTION_TOKEN: string
     API_SECRET_KEY: string
-    SEND_GRID_API_KEY: string
-    SEND_EMAIL_FROM: string
-    SEND_EMAIL_TO: string
     VERCEL_ENV: 'production' | 'development' | 'preview'
   }
 }
