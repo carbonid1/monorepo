bbc77c77ccd47b9c745a3482c2bbfb30d699e6ab
Andrew <carbonid1@gmail.com>
2023-02-27 22:50:47 +0200

    upd

--- apps/book-hub/app/authors/page.tsx
+++ apps/book-hub/app/authors/page.tsx
@@ -3,7 +3,7 @@ import prisma from 'lib/prisma'
 type FetchAuthors = () => Promise<ReturnType<typeof prisma.author.findMany>>
 
 const fetchAuthors: FetchAuthors = async () => {
-  const res = await fetch(`${process.env.VERCEL_URL}/api/authors`)
+  const res = await fetch(`https://${process.env.VERCEL_URL}/api/authors`)
   const { authors } = await res.json()
   return authors
 }
