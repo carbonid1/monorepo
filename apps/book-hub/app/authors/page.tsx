4fefc600bb2e4e7d6aa9e16f695e9b86655691bd
Andrew Korin <36999873+carbonid1@users.noreply.github.com>
2023-03-01 02:13:38 +0200

    feat: author page (#203)

--- apps/book-hub/app/authors/page.tsx
+++ apps/book-hub/app/authors/page.tsx
@@ -1,8 +1,27 @@
-const Page = () => {
+import prisma from 'lib/prisma'
+
+const getAuthors = () => prisma.author.findMany()
+
+const Page = async () => {
+  const authors = await getAuthors()
+
   return (
-    <div className="prose">
-      <h1>Authors Page</h1>
-    </div>
+    <table className="table w-full mt-8">
+      <thead>
+        <tr>
+          <th></th>
+          <th>Name</th>
+        </tr>
+      </thead>
+      <tbody>
+        {authors.map((author, index) => (
+          <tr key={author.id}>
+            <th>{index}</th>
+            <td>{author.name}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
   )
 }
 
