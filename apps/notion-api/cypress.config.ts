b6012d80015989f343999bf94d7bdfed5926a7ea
Andrew Korin <36999873+carbonid1@users.noreply.github.com>
2023-03-07 10:20:19 +0200

    build: turborepo v1.8 (#207)
    * init
    
    * 1.8 init
    
    * prisma and lint upds
    
    * upd

--- apps/notion-api/cypress.config.ts
+++ apps/notion-api/cypress.config.ts
@@ -1,10 +1,11 @@
-import { defineConfig } from "cypress";
+import { defineConfig } from 'cypress'
 
 export default defineConfig({
+  video: false,
   component: {
     devServer: {
-      framework: "next",
-      bundler: "webpack",
+      framework: 'next',
+      bundler: 'webpack',
     },
   },
-});
+})
